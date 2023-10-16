42892aac16f6a9421b841f992c1a361e51afcaa1
moeka kitamura <moeka.k76@gmail.com>
2023-10-16 23:08:12 +0900

    🎨new design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,11 @@ export const Header = () => {
   return (
     <div>
       <header>
-        <h1>Trelloみたいなやつ</h1>
+        <h1>
+          <i className="fa-regular fa-clipboard"></i>
+          <span className="mx-20">TODO APP</span>
+          <i className="fa-regular fa-clipboard"></i>
+        </h1>
       </header>
     </div>
   );
